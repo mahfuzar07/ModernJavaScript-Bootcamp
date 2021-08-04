ce5c5712b9e850386dd484110f15bceff480c811
Md.Mahfuzar Rahman <58438084+mahfuzar07@users.noreply.github.com>
2021-08-05 03:26:46 +0600

    Create todo.js

---
+++ Array/todo.js
@@ -0,0 +1,14 @@
+const todos = [
+  'Order cat food',
+  'Clean kitchen',
+  'Buy food',
+  'Do work',
+  'Exercise',
+];
+console.log(`You have ${todos.length} todos!`);
+console.log(`Todo 01: ${todos[0]}`);
+console.log(`Todo 04: ${todos[todos.length - 1]}`);
+
+//create an array with five todos
+//you have x todos
+//print the first and scoond to last items -->
