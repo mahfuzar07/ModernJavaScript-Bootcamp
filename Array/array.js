08108119ae5c69eed6d1e9d1eaf89bb4dda4d2ae
Md.Mahfuzar Rahman <58438084+mahfuzar07@users.noreply.github.com>
2021-08-05 03:26:40 +0600

    Create array.js

---
+++ Array/array.js
@@ -0,0 +1,15 @@
+//Create  Atray
+const notes = ['Note 01', 'Note 02', 'Note 03'];
+
+console.log(notes.pop()); //remove array in last item by pop
+notes.push('My New Note'); //array last item add in array by push
+
+console.log(notes.shift());
+notes.unshift('My First Note');
+
+notes.splice(1, 0, 'This is New Second Item'); //target index remove/without_remove replace/add item
+
+console.log(notes.length); // Array item Count
+console.log(notes); // All Array item
+console.log(notes[0]); //Access an Array item using the index position
+console.log(notes[notes.length - 1]); //Access an Array last item using the index position
